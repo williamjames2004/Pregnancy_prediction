4f6758857705f9404e9d539845801a304a699cc1
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-06-27 12:45:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -332,4 +332,5 @@ app.register_blueprint(user_routes, url_prefix="/users")
 # Run App
 # ----------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
